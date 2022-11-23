dc890e4eeff9af330d9ccbf91e333d5f76de69b2
nikolaipaukkonen <nikolai.paukkonen@helsinki.fi>
2022-11-23 23:31:55 +0200

    testi 12

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -80,3 +80,13 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(len(self.kori.ostokset()), 1)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostos_jolla_oikea_maara_ja_nimi(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.lukumaara(), 2)
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
